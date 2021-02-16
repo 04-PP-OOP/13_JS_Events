68a597b5d24d546f1fbb4dfc71e605832a7808a7
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-02-16 15:05:15 +0100

    update CSS / day night + JS switchToDay/Night

--- events.js
+++ events.js
@@ -4,12 +4,19 @@ function ausgabe(outputStr) {
     console.log(outputStr);
 }
 
+/** Definition DOM-Elemente */
+item_body = document.body;
+item_h1 = document.body.children[0];
+item_p = document.body.children[1];
+
+// ausgabe(item_body);
+// ausgabe(item_h1);
+// ausgabe(item_p);
 
 /** Definition Btn */
 let btn;
 btn = document.getElementById("trigBtn");
-//btn = document.body;
-//ausgabe(btn);
+
 
 /** Event-Überwachung, hier: KLick! */
 btn.addEventListener("click", actOnClick);
@@ -18,5 +25,24 @@ btn.addEventListener("click", actOnClick);
 //actOnClick();
 function actOnClick() {
     ausgabe("hier wurde geklickt!");
+
+    if (true) { // ??
+        switchToNightMode();
+    } else {
+        switchToDayMode()
+    }
+}
+
+function switchToDayMode() {
+    item_body.className = "day";
+    item_h1.className = "day";
+    item_p.className = "day";
+}
+
+//switchToNightMode();
+function switchToNightMode() {
+    item_body.className = "night";
+    item_h1.className = "night";
+    item_p.className = "night";
 }
 
