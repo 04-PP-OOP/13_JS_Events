b413eb2a33a58f882b7944f378feedba3ea593f5
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-02-16 13:00:21 +0100

    update JS / Event , CSS btn

--- events.js
+++ events.js
@@ -3,3 +3,20 @@
 function ausgabe(outputStr) {
     console.log(outputStr);
 }
+
+
+/** Definition Btn */
+let btn;
+btn = document.getElementById("trigBtn");
+//btn = document.body;
+//ausgabe(btn);
+
+/** Event-Überwachung, hier: KLick! */
+btn.addEventListener("click", actOnClick);
+
+
+//actOnClick();
+function actOnClick() {
+    ausgabe("hier wurde geklickt!");
+}
+
